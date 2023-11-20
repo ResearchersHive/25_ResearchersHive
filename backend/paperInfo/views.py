45da190d5e6f56deeae78c25f4e82a5b9b5d7f2d
PradhyumnaPalore <pradhyumnapalore@gmail.com>
2023-11-20 13:34:12 +0530

    Using stopword List

--- backend/paperInfo/views.py
+++ backend/paperInfo/views.py
@@ -8,11 +8,12 @@ from .serializers import PaperInfoSerializer
 import nltk
 # nltk.data.path.append("../NLTK")
 # from nltk import pos_tag
-from nltk.corpus import stopwords
+# from nltk.corpus import stopwords
 from nltk import FreqDist
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 
 import spacy
+stopWords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
 # Load the English language model from spaCy
 nlp = spacy.load("en_core_web_sm")
@@ -70,7 +71,7 @@ def extract_keywords(abstract):
     words = abstract.split()
 
     # Remove stopwords (common words that usually don't carry much meaning)
-    stop_words = set(stopwords.words('english'))
+    stop_words = stopWords
     words = [word.lower() for word in words if word.isalpha() and word.lower() not in stop_words]
 
     # Part-of-speech tagging to identify nouns and adjectives
