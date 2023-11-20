8edfcc2953e0575f864ce0d894fa50f5baf71448
2023201053 <141819395+2023201053@users.noreply.github.com>
2023-11-20 13:45:05 +0530

    Add Paper Page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,30 +1,37 @@
-import { Link } from 'react-router-dom'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import './Navbar.css'
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const CustomNavbar = () => {
+  return (
+    <header>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container fluid>
+          <Navbar.Brand href="#">Researchers&rsquo; Hive</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 
-    return (
-        <header>
-            <Navbar className="navB" expand="lg" variant="dark">
-            {/* <Container fluid> */}
-                <Navbar.Brand style={{ color: 'aliceblue' }}>Researcher's Hive</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto justify-content-end" style={{ width: "100%" }}>
-                        <Nav.Link href="#home">Research</Nav.Link>
-                        <Nav.Link href="#link">Publications</Nav.Link>
-                        <Nav.Link href="#link">Search</Nav.Link>
-                        <Button variant="outline-success">Login</Button>
-                    </Nav>
-                </Navbar.Collapse>
-            {/* </Container> */}
-        </Navbar>
-        </header>
-    )
-}
-
-export default CustomNavbar
+export default CustomNavbar;
